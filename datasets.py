0f765f968d826513d1b21e631327d16bb716b8b9
Frederik Baggesen <201804495@post.au.dk>
2021-10-18 14:16:27 +0200

    standard autoencoder

--- datasets.py
+++ datasets.py
@@ -1,13 +1,16 @@
 import os
+import torch
+import random
 from torchvision.io import read_image
-from torchvision import transforms
+from torchvision import transforms as T
 from torch.utils.data import Dataset
 
 
 class AnimalImageDataset(Dataset):
-    def __init__(self, img_dir, size = None):
+    def __init__(self, img_dir, image_size = None, data_size = None):
         self.img_dir = img_dir
-        self.size = size
+        self.image_size = image_size
+        self.data_size = data_size
 
         totalImages = 0
         for _, _, files in os.walk(img_dir):
@@ -18,6 +21,9 @@ class AnimalImageDataset(Dataset):
         self.noOfImages = totalImages
 
     def __len__(self):
+
+        if self.data_size and self.data_size <= self.noOfImages:
+            return self.data_size
         return self.noOfImages
 
     def __getitem__(self, index):
@@ -25,8 +31,49 @@ class AnimalImageDataset(Dataset):
         img_path = os.path.join(self.img_dir, "animal " + img_index + ".jpg")
         image = read_image(img_path)
 
-        if self.size:
-            image = transforms.Compose([transforms.Resize(self.size)])(image)
+        if self.image_size:
+            image = T.Resize(self.image_size)(image)
+        
+        image = preprocessImage(image)
 
         return image
 
+def preprocessImage(image):
+    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    image = (lambda x: x/255)(image)
+    image = image.to(dev)
+    return image
+
+def getSpecificImage(index, image_size):
+    img_index = "(" + str(index) + ")"
+    img_path = os.path.join("data/val", "animal " + img_index + ".jpg")
+    image = read_image(img_path)
+    image = preprocessImage(image)
+    if image_size:
+        image = T.Resize(image_size)(image)
+    return image.unsqueeze(0)
+
+
+def getCat(image_size = None):
+    return getSpecificImage(61, image_size)
+
+def getDog(image_size = None):
+    return getSpecificImage(96, image_size)
+
+def getFox(image_size = None):
+    return getSpecificImage(219, image_size)
+
+def getWolf(image_size = None):
+    return getSpecificImage(555, image_size)
+
+def getLeopard(image_size = None):
+    return getSpecificImage(590, image_size)
+
+def getTiger(image_size = None):
+    return getSpecificImage(491, image_size)
+
+def getLion(image_size = None):
+    return getSpecificImage(565, image_size)
+
+def getRandom(image_size = None):
+    return getSpecificImage(random.randint(1, 1500), image_size)
--- testing.py
+++ testing.py
@@ -1,20 +1,58 @@
 import torch
-import numpy as np
-import matplotlib.pyplot as plt
-import torchvision
-from datasets import AnimalImageDataset
+from torch import optim
+import visualizing as Vis
+import datasets as Data
+from models import AutoEncoder, AutoEncoder2, AutoEncoder3, AutoEncoder4, AutoEncoder5
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import utility as U
+
+#Setup GPU
+dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+img_size = (256, 256)
 
 #Load Data
-animal_dataset = AnimalImageDataset(img_dir = 'data/train', size = (256,256))
-loader = DataLoader(dataset = animal_dataset, batch_size = 64, shuffle = True)
+animal_dataset_train = Data.AnimalImageDataset(img_dir = 'data/train', image_size = img_size)
+animal_dataset_val = Data.AnimalImageDataset(img_dir = 'data/val', image_size = img_size)
+loader_train = DataLoader(dataset = animal_dataset_train, batch_size = 64, shuffle = True)
+loader_val = DataLoader(dataset = animal_dataset_val, batch_size = 64, shuffle = False)
 
-train_features = next(iter(loader))
-print(train_features.size())
+#Setup Model
+model = AutoEncoder3()
+model.to(dev)
+opt = optim.SGD(model.parameters(), lr = 0.01, momentum=0.9)
 
-img = train_features[0]
+U.load_model_state(model, opt, 'trained_models/autoencoder6.pt')
 
-grid_img = torchvision.utils.make_grid(train_features[0:10], nrow=5)
+#history = U.fit_model(loader_train, model, F.mse_loss, opt, 5, loader_val)
+#U.save_model_state(model, opt, 'trained_models/autoencoder9.pt')
+#Vis.displayHistory(history)
 
-plt.imshow(grid_img.permute(1, 2, 0))
-plt.show()
+catImage = Data.getRandom(image_size = img_size)
+foxImage = Data.getRandom(image_size = img_size)
+
+catEncoding = model.encode(catImage)
+foxEncoding = model.encode(foxImage)
+catDecoding = model.decode(catEncoding)
+foxDecoding = model.decode(foxEncoding)
+
+hybridEncoding1 = torch.lerp(catEncoding, foxEncoding, 0.25)
+hybridEncoding2 = torch.lerp(catEncoding, foxEncoding, 0.5)
+hybridEncoding3 = torch.lerp(catEncoding, foxEncoding, 0.75)
+
+hybrid1 = model.decode(hybridEncoding1)
+hybrid2 = model.decode(hybridEncoding2)
+hybrid3 = model.decode(hybridEncoding3)
+
+merging = torch.cat((catDecoding, hybrid1, hybrid2, hybrid3, foxDecoding),0)
+Vis.displayImages(merging, 1)
+
+
+#To do:
+# - Clean models file
+# - Create deepdream model
+# - Clean up test file
+# - Variational autoencoder
+# - Choose architecture for standard ae
+# - Better saving system (Include image_size & Loss)
